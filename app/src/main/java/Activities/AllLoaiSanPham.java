dbf075f7a739896268ae5db42617219db6d36603
huylam10 <nhlam151003@gmail.com>
2024-03-23 00:31:30 +0700

    update

---
+++ app/src/main/java/Activities/AllLoaiSanPham.java
@@ -0,0 +1,16 @@
+package Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nhom6_pro1121_md18402.R;
+
+public class AllLoaiSanPham extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_loai_san_pham);
+    }
+}
